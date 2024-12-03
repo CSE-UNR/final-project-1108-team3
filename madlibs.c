21b36b505cf2db20ad70a1f9736673b6a6129d0a
1108-Wall-Liam <liamwall@unr.edu>
2024-12-03 23:50:42 +0000

    liams functions second time

--- madlibs.c
+++ madlibs.c
@@ -3,27 +3,28 @@
 //Madlibs
 #include <stdio.h>
 #define INPUT "madlib2.txt"
-#define SIZE 100
-char readFile(FILE* fPtr);
+#define STRING 100
+#define SIZE 8
+void readFile(FILE* fPtr, char array1[], int array1Size);
 void storeString(char stringArray[], int arraySize);
 int main(){
 	//creating variables
 	FILE* fp;
 	char y;//this holds the value for A, N, or V
-	char array[SIZE]; //this holds the string that the user enters
+	char array[STRING], readArray[SIZE]; //this holds the string that the user enters
 	fp = fopen(INPUT, "r");
 	if (fp == NULL){
 		printf("no work\n");
 		return 0;
 	}
 	
-	y = readFile(fp);
-	storeString(array, SIZE);
+	readFile(fp, readArray, SIZE);
+	storeString(array, STRING);
 	fclose(fp);
 	return 0;
 }
 /*This function goes thru the file until it reaches an A, N, or V, then it returns the value. So it will only continue to work for the whole program if we immediately replace the A, N, or V with the user input (which I think is what we talked about). If you have any questions or need me to work on the function more just text me. */
-char readFile(FILE* fPtr){
+void readFile(FILE* fPtr, char array1[], int array1Size){
 	char x;
 	int i = 0, k = 0;
 	while (fscanf(fPtr, "%c", &x) == 1){
@@ -33,7 +34,8 @@ char readFile(FILE* fPtr){
 	for(int j = 0; j < i; j++){
 		fscanf(fPtr, "%c", &x);
 		if(x == 'A' || x == 'N' || x == 'V'){
-			return x;
+			array1[k] = x;
+			k++;
 		}
 	}
 }
